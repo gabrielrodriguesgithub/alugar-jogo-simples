decdfcc811d2e2fa77d6f44980743736c88b3dda
Gabriel Rodrigues <biel.rodriguesvieira@gmail.com>
2023-10-21 14:25:44 -0300

    first commit

---
+++ js/app.js
@@ -0,0 +1,16 @@
+function alterarStatus(id){
+    let gameClicado = document.getElementById(`game-${id}`);
+    let imagem = gameClicado.querySelector('.dashboard__item__img');
+    let botao = gameClicado.querySelector('.dashboard__item__button');
+
+    if(botao.classList.contains("dashboard__item__button--return")) {
+        botao.classList.remove("dashboard__item__button--return");
+        imagem.classList.remove("dashboard__item__img--rented");
+        botao.textContent = 'Alugar'
+    } else {
+        botao.classList.add("dashboard__item__button--return");
+        imagem.classList.add("dashboard__item__img--rented");
+        botao.textContent = 'Devolver'
+    }
+
+}
